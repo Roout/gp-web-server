22492e3a78eaf0e69669883ebf03283f6ae2332d
Roout <following2future@gmail.com>
2021-12-11 18:02:27 +0300

    test simple GET  parser

--- main.c
+++ main.c
@@ -74,9 +74,39 @@ int main() {
     return 0;
 }
 
+typedef struct {
+    char *method;   // GET or POST
+    char *path;     // /path/to/file
+    char *protocol; // HTTP/1.1
+} RequestHeader;
+
+/**
+ * @param data null-terminated string
+ * @param header parsing result on success
+ * @return 0 on success otherwise -1
+*/
+int ParseHeader(char* data, RequestHeader* header) {
+    header->method = strtok(data, " ");
+    if (header->method == NULL) {
+        return -1;
+    }
+    header->path = strtok(NULL, " ");
+    if (header->path == NULL) {
+        return -1;
+    }
+    header->protocol = strtok(NULL, " ");
+    if (header->protocol == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+#define CRLF "\r\n"
+
 void HandleClient(int fd) {
+    // global buffer for this client
     char buffer[BUFFER_SIZE + 1];
-    char *pattern = "\r\n";
+    char *pattern = CRLF;
 
     BufferState state {
         .buffer = buffer,
@@ -84,17 +114,58 @@ void HandleClient(int fd) {
         .capacity = BUFFER_SIZE
     };
 
-    size_t pattern_len = strlen(pattern);
-    char *match = NULL;
-    while ((match = read_until(fd, &state, pattern)) != NULL) {
-        size_t consumed = match - state.buffer;
-        *match = '\0';
-        printf("read_until: %s\n", state.buffer);
-        assert(state.size >= consumed + pattern_len);
-        chop_left(&state, consumed + pattern_len);
+    // read header
+    char *match = read_until(fd, state, pattern);
+    if (match == NULL) {
+        printf(stderr, "Failed to read header.\n");
+        exit(1);
     }
-    if (size > 0) {
-        printf("read_until: %s\n", free_buffer);
+    // header is the line: [buffer, match)
+    *match = '\0';
+    char *line = state->buffer;
+    // update state
+    chop_left(state, (match - line) + strlen(pattern));
+
+    RequestHeader header;
+    if (ParseHeader(line, &header) < 0) {
+        // TODO: handle error
+        close(fd);
+        exit(1);
     }
-    (void) close(fd);
+
+    if (strcmp(header->method, "GET")) {
+        while (match != NULL) {
+            match = read_until(fd, state, CRLF);
+            if (match == NULL) {
+                // TODO: handle error
+                close(fd);
+                exit(1);
+            }
+            if (state->buffer == match) {
+                // meet the combination CRLF CRLF
+                break;
+            }
+            char *line = state->buffer;
+            chop_left(state, (match - line) + strlen(pattern));
+            char *field = strtok(line, ": ");
+            char *value = strtok(NULL, "\r\n");
+            assert(*match == '\0');
+            // *match = '\0';
+            printf("%s:%s\n", field, value);
+        }
+        // TODO: check what answer is needed in rfc
+        char default_answer[] = "HTTP/1.1 200 OK" CRLF "Content-Length: 0" CRLF CRLF;
+        write_some(fd, default_answer, strlen(default_answer));
+    }
+    else if (strcmp(header->method, "POST")) {
+        // TODO: check what answer is needed in rfc
+        char default_answer[] = "HTTP/1.1 404 Not Found" CRLF "Content-Length: 0" CRLF CRLF;
+        write_some(fd, default_answer, strlen(default_answer));
+    }
+    else {
+        char default_answer[] = "HTTP/1.1 404 Not Found" CRLF "Content-Length: 0" CRLF CRLF;
+        write_some(fd, default_answer, strlen(default_answer));
+    }
+    
+    close(fd);
 }
