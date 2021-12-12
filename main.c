a13ff8eaa4cedd82cc75307105e0b30e32ffb402
Roout <following2future@gmail.com>
2021-12-13 01:17:23 +0300

    add forking

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 #include <assert.h>
 #include <string.h> 
 
-#include <unistd.h>		// close, fork
+#include <unistd.h>	    // close, fork
+#include <sys/wait.h>   // wait
 
 #include "iowrap.h"
 #include "server.h"
 
 #define HOST "127.0.0.1"
-#define PORT "18000"
+#define PORT "18001"
+// size of listen's queue
 #define BACKLOG 64
+#define MAX_CONNECTIONS 100
 
 // client's buffer size used for reading incoming requests
 // if the request will be longer than this size then 
@@ -21,33 +24,91 @@
 // handle client with provided socket descriptor `fd`
 static void handle_client(int fd);
 
+static int handle_client_pid(pid_t pid, int wstatus, size_t *proccess_count);
+
+static int wait_slots(size_t *proccess_count);
+
 int main() {
     printf("Server started at %shttp://%s:%s%s\n", "\033[92m", HOST, PORT, "\033[0m");
 
     Server server;
     init_server(&server, HOST, PORT, BACKLOG);
 
-    // ignore SIGCHLD to avoid zombie threads
-    signal(SIGCHLD, SIG_IGN);
+    // number of not-terminated proccesses
+    size_t proccess_count = 0;
     // accept clients
     while (1) {
+        if (wait_slots(&proccess_count) < 0) {
+            // failed to terminate a proccess
+            exit(EXIT_FAILURE);
+        }
 		int client_fd = accept_client(&server);
         int pid = fork();
-        if (pid < 0) {
+        if (pid < 0) { // error
             fprintf(stderr, "Failed to create a child proccess for the new client.\n");
             fprintf(stderr, "Reason: %s\n", strerror(errno));
             close(client_fd);
         }
-        else if (pid == 0) {
+        else if (pid == 0) { // here goes child proccess's branch
             handle_client(client_fd);
             exit(EXIT_SUCCESS);
         }
-        else {
+        else { // parent's branch
+            // proccess was created successfully
+            proccess_count++;
             // parent proccess
-            // close the fd because the child should own it now
+            // close the fd because only the child should own it now
             close(client_fd);
         }
     }
+    return 0;
+}
+
+static int handle_client_pid(pid_t pid, int wstatus, size_t *proccess_count) {
+    assert(proccess_count);
+    if (pid == -1) {
+        fprintf(stderr, "waitpid failed.\n");
+        fprintf(stderr, "Reason: %s\n", strerror(errno));
+        return -1;
+    }
+    // proccess was terminated
+    assert(*proccess_count > 0);
+    *proccess_count--;
+    if (!WIFEXITED(wstatus)) {
+        // child isn't terminated normally by exit(...)
+        // log this to stderr
+        fprintf(stderr, "Child %d isn't terminated normally\n", pid);
+    }
+    return 0;
+}
+
+static int wait_slots(size_t *proccess_count) {
+    assert(proccess_count);
+    if (*proccess_count < MAX_CONNECTIONS) {
+        return 0;
+    }
+    // otherwise hang server until there will be free slot
+    printf("There is already %zu connections. Wait for free slot...\n", *proccess_count);
+    int wstatus = 0;
+    pid_t pid = 0;
+    // Try to handle as many terminated proccesses as possible in non-blocking mode
+    while ((pid = waitpid(-1, &wstatus, WNOHANG)) != 0) {
+        // handle success or error in function `handle_client_pid`
+        if (handle_client_pid(pid, wstatus, proccess_count) < 0) {
+            return -1;
+        }
+    }
+    // if still no free connections -> block until there will be one
+    if (*proccess_count == MAX_CONNECTIONS) {
+        wstatus = 0;
+        // default option = 0 (last arg)
+        // because it waits only for terminated children
+        pid = waitpid(-1, &wstatus, 0);
+        if (handle_client_pid(pid, wstatus, proccess_count) < 0) {
+            return -1;
+        }
+    }
+    // success
     return 0;
 }
 
